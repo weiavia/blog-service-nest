dd406f9fd13fd3f8747e34b5de83e4fcfc382490
weiavia <you@example.com>
2019-06-13 17:21:54 +0800

    comment modify

--- src/helpers/Enum.ts
+++ src/helpers/Enum.ts
@@ -2,7 +2,7 @@ export const ARTICLE = 1
 export const MUSIC = 2
 export const VIDEO = 3
 
-export const COMMENT_PAGE_TAKE = 1
+export const COMMENT_PAGE_TAKE = 10
 
 export const PRAISE_TYPE = {
   BLOCk: 1,
